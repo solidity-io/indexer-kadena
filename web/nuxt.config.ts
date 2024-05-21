cfc2f12026a0aafc73b2891a82b7b96bf8857436
1mateus <mateus4loading@gmail.com>
2024-05-21 10:35:05 -0300

    feat(web): use outside directive

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   ],
 
   build: {
-    transpile: ['nuxt-graphql-request'],
+    transpile: ['nuxt-graphql-request', '@venegrad/vue3-click-outside'],
   },
 
   alias: {
