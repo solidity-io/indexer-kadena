c41bb1747c7509d487a0deaa8075e95019278366
1mateus <mateus4loading@gmail.com>
2024-05-20 23:01:04 -0300

    feat(web): base search feature

--- web/app.vue
+++ web/app.vue
@@ -20,4 +20,15 @@ import '@fontsource/roboto/latin.css'
   html {
     height: 100%;
  }
+
+ .scrollbar-custom::-webkit-scrollbar {
+    -webkit-appearance: none !important;
+    width: 8px !important;
+  }
+
+  .scrollbar-custom::-webkit-scrollbar-thumb {
+    border-radius: 20px !important;
+    background-color: #202327cc !important;
+    -webkit-box-shadow: 0 0 1px rgba(255, 255, 255, 0.5) !important;
+  }
 </style>
--- web/components/Search.vue
+++ web/components/Search.vue
@@ -1,71 +1,219 @@
 <script setup lang="ts">
-const searchItems = [
-  {
+import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
+import { gql } from 'nuxt-graphql-request/utils';
+import { debounce } from 'chart.js/helpers';
+
+const getBlocksQuery = gql`
+  query SearchBlocks($hash: String) {
+    allBlocks(last: 4, filter: {or: {hash: {includes: $hash}}}) {
+      nodes {
+        id
+        hash
+        nodeId
+        height
+      }
+    }
+    allTransactions(last: 4, filter: {or: {requestkey: {includes: $hash}}}) {
+      nodes {
+        id
+        nodeId
+        requestkey
+      }
+    }
+  }
+`
+
+const data = reactive({
+  query: '',
+  open: false,
+  loading: false,
+  searched: null,
+  filter: {
     value: 'all',
     label: 'All filters',
   },
-  {
-    value: 'addresses',
-    label: 'Addresses',
-  },
-  {
-    value: 'tokens',
-    label: 'Tokens',
-  },
-  {
-    value: 'name-tags',
-    label: 'Name Tags',
-  },
-  {
-    value: 'domain-names',
-    label: 'Domain Names',
-  },
-  {
-    value: 'labels',
-    label: 'Labels',
-  },
-]
+  filters: [
+    {
+      value: 'all',
+      label: 'All filters',
+    },
+    {
+      value: 'blocks',
+      label: 'Blocks',
+    },
+    {
+      value: 'transactions',
+      label: 'Transactions',
+    },
+  ]
+})
 
-const value = ref(searchItems[0])
+const { $graphql } = useNuxtApp();
+
+const isEmpty = computed(() => {
+  if (data.searched === null) {
+    return true
+  }
+
+  return Object.values(data.searched).every(({ nodes }: any) => nodes?.length === 0)
+})
+
+const search = debounce(async (value: string[]) => {
+  // if (data.loading) {
+  //   return
+  // }
+
+  if (!value[0]) {
+    data.searched = null
+
+    return
+  }
+
+  data.loading = true
+
+  const res = await $graphql.default.request(getBlocksQuery, {
+    hash: value[0],
+  });
+
+  if (value[0] === data.query) {
+    data.searched = res
+    data.loading = false
+  }
+}, 500)
+
+const cleanup = () => {
+  // data.query = ''
+  // data.searched = null
+  console.log('kekw')
+  data.open = false
+}
 </script>
 
 <template>
   <div
-    class="
-      flex gap-2 items-center
-      w-full p-2 bg-gray-800 rounded-lg bazk:max-w-[700px]
-    "
+    class="relative"
+    v-outside="cleanup"
   >
     <div
-      class="hidden bazk:block"
+      @click.prevent="data.open = true"
+      :class="[data.open && 'border-kadscan-500']"
+      class="
+        flex gap-2 items-center
+        w-full p-2 bg-gray-800 rounded-lg bazk:max-w-[700px] border border-transparent
+      "
     >
-      <Select
-        v-model="value"
-        :items="searchItems"
+      <div
+        class="hidden bazk:block"
+      >
+        <Select
+          v-model="data.filter"
+          :items="data.filters"
+        />
+      </div>
+
+      <input
+        class="
+          px-1
+          bazk:px-2
+          py-2
+          text-sm
+          bg-transparent
+          outline-none
+          h-full w-full
+          text-font-400
+          placeholder:text-font-500
+        "
+        v-model="data.query"
+        @input="search(($event.target as any).value)"
+        placeholder="Search by Address / Token / Block"
       />
+
+      <div
+        class="mr-1 flex items-center justify-center p-[6px] bg-gray-500 rounded-lg h-8 w-8 bazk:h-9 bazk:w-9 shrink-0"
+      >
+        <IconSearch
+          class="w-5 h-5"
+        />
+      </div>
     </div>
 
-    <input
-      class="
-        px-1
-        bazk:px-2
-        py-2
-        text-sm
-        bg-transparent
-        outline-none
-        h-full w-full
-        text-font-400
-        placeholder:text-font-500
-      "
-      placeholder="Search by Address / Token / Block"
-    />
-
     <div
-      class="mr-1 flex items-center justify-center p-[6px] bg-gray-500 rounded-lg h-8 w-8 bazk:h-9 bazk:w-9 shrink-0"
+      v-if="data.open"
+      class="absolute top-full mt-2 w-full max-w-[700px] bg-gray-700 rounded-lg p-4 gap-4 flex flex-col border border-gray-300 max-h-[344px] overflow-auto scrollbar-custom"
     >
-      <IconSearch
-        class="w-5 h-5"
-      />
+      <div
+        v-if="data.loading"
+        class="text-sm text-white"
+      >
+        Loading...
+      </div>
+
+      <span
+        class="text-white text-sm"
+        v-else-if="isEmpty"
+      >
+        Nothing found.
+      </span>
+
+      <template
+        v-else
+      >
+        <div
+          v-if="data?.searched?.allBlocks?.nodes?.length > 0"
+          class="flex flex-col"
+        >
+          <div
+            class="pb-2"
+          >
+            <span
+              class="text-white text-sm"
+            >
+              Blocks
+            </span>
+          </div>
+
+          <div
+            class="py-3 border-b border-gray-300"
+            :key="'block:'+block.id"
+            v-for="block in data.searched?.allBlocks?.nodes"
+          >
+            <NuxtLink
+              class="text-white text-sm"
+              :to="`/blocks/${block.nodeId}`"
+            >
+              {{ block.hash }}
+            </NuxtLink>
+          </div>
+        </div>
+
+        <div
+          v-if="data?.searched?.allTransactions?.nodes?.length > 0"
+          class="flex flex-col gap-2"
+        >
+          <div
+            class="pb-2"
+          >
+            <span
+              class="text-white text-sm"
+            >
+              Transactions
+            </span>
+          </div>
+
+          <div
+            class="py-3 border-b border-gray-300"
+            :key="'block:'+transaction.id"
+            v-for="transaction in data.searched?.allTransactions?.nodes"
+          >
+            <NuxtLink
+              class="text-white text-sm"
+              :to="`/transactions/${transaction.nodeId}`"
+            >
+              {{ transaction.requestkey }}
+            </NuxtLink>
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
