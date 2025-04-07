cbdb3cc9e3516907202a6bc0b362b3c1b8ac4043
Marcos Schead <wssmarcos@gmail.com>
2025-04-07 16:31:14 -0300

    tests: added for transaction query

---
+++ indexer/tests/integration/fixtures/transaction/transaction.fixture.001.ts
@@ -0,0 +1,21 @@
+export const transactionFixture001 = {
+  data: {
+    transaction: {
+      id: 'VHJhbnNhY3Rpb246WyJpcDRjMEhkYVJ6OERkTEZqRFJFbUE1RTJheVFUcE9NWmRxcWN6c3FKUDZzIiwiSmV2YTlHOXlDMFdLT3ZaUFMwVmFtY1ZwNXdKcU9PWFpXNGpkbG5JY0U5ayJd',
+      hash: 'Jeva9G9yC0WKOvZPS0VamcVp5wJqOOXZW4jdlnIcE9k',
+      cmd: {
+        meta: {
+          sender: 'k:837372d7d49c2a318ecbc215cd19f8bfcbd2162a86cec870e8831b2189fc8d9f',
+        },
+        payload: {
+          code: '"(free.radio02.close-send-receive \\"k:74ecf37948b42c31dba4793380857f1c8a4fb5c47843fbad67d38d436fc03054\\" [{\\"address\\":\\"k:bf8718bb4234abde93609b766d6ff5f1034da7475daa9720575c94ddba90a752\\",\\"mic\\":\\"598935a7\\",\\"gatewayId\\":\\"e87829fffe1d66fb\\"},{\\"address\\":\\"k:0225d5c06e188c1d3d48c34a9688b66e1c66e57247b443cbcd6e0ff00e798393\\",\\"mic\\":\\"598935a7\\",\\"gatewayId\\":\\"e87829fffe1d66fb\\"}] [] )"',
+        },
+      },
+      result: {
+        badResult: null,
+        goodResult: '"Write succeeded"',
+        continuation: null,
+      },
+    },
+  },
+};
