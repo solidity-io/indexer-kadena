a81a0d60ae62f070270b780e03285a888247cea1
Marcos Schead <wssmarcos@gmail.com>
2025-04-17 08:49:22 -0300

    tests: added for graph configuration

---
+++ indexer/tests/integration/graph-configuration.test.ts
@@ -0,0 +1,14 @@
+import { GraphQLClient } from 'graphql-request';
+import { getGraphConfigurationQuery } from './builders/graph-configuration.builder';
+import { graphConfigurationFixture001 } from './fixtures/graph-configuration/graph-configuration.fixture.001';
+
+const client = new GraphQLClient(process.env.API_URL ?? 'http://localhost:3001/graphql');
+
+describe('Graph Configuration', () => {
+  it('#001', async () => {
+    const query = getGraphConfigurationQuery();
+    const data = await client.request(query);
+
+    expect(graphConfigurationFixture001.data).toMatchObject(data);
+  });
+});
